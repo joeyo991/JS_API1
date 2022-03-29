a62a3e491ffe5e7854f696956915a8266b0bfcce
joeyo991 <joeyosull@gmail.com>
2022-03-29 14:17:35 +0000

    Making a Request Part 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,15 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = `Your key is valid until<br>${data.expiry}`;
+
+    resultsModal.show();
 }
